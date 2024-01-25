aa26558aa02a89095924ddd192e2d4d778e174a1
PPiinngg <glyphli@protonmail.com>
2024-01-25 10:04:02 +0000

    3rd order interpolations

--- src/math/interpolation.zig
+++ src/math/interpolation.zig
@@ -1,22 +1,31 @@
 const std = @import("std");
 
+// Implementations translated from the below paper by Olli Niemitalo (the GOAT)
+// https://yehar.com/blog/wp-content/uploads/2009/08/deip.pdf
+
 pub fn linear(comptime T: type, t: T, x0: T, x1: T) T {
     return x0 + t * (x1 - x0);
 }
 
-/// Page 36 https://yehar.com/blog/wp-content/uploads/2009/08/deip.pdf
 pub fn hermite(comptime T: type, t: T, x_1: T, x0: T, x1: T, x2: T) T {
-    const t_pow_2 = t * t;
-    const t_pow_3 = t * t * t;
-    return (x_1 * ((t_pow_2 - (t * 0.5)) - (t_pow_3 * 0.5))) +
-        (x0 * (1 - (t_pow_2 * 2.5) + (t_pow_3 * 1.5))) +
-        (x1 * ((t * 0.5) + (t_pow_2 * 2) - (t_pow_3 * 1.5))) +
-        (x2 * ((t_pow_3 * 0.5) - (t_pow_2 * 0.5)));
+    const c1: T = 1.0 / 2.0 * (x1 - x_1);
+    const c2: T = x_1 - (5.0 / 2.0) * x0 + 2.0 * x1 - (1.0 / 2.0) * x2;
+    const c3: T = 1.0 / 2.0 * (x2 - x_1) + 3.0 / 2.0 * (x0 - x1);
+    return ((c3 * t + c2) * t + c1) * t + x0;
 }
 
-// pub fn hermite(comptime T: type, t: T, x_1: T, x0: T, x1: T, x2: T) T {
-//     const y1 = 0.5 * (x1 - x_1);
-//     const y2 = x_1 - ((2.5 * x0) + (2 * x1)) - (0.5 * x2);
-//     const y3 = 0.5 * (x2 - x_1) + 1.5 * (x0 - x1);
-//     return ((y3 * t + y2) * t * y1) * t + x0;
-// }
+pub fn lagrange(comptime T: type, t: T, x_1: T, x0: T, x1: T, x2: T) T {
+    const c1: T = x1 - 1.0 / 3.0 * x_1 - 1.0 / 2.0 * x0 - 1.0 / 6.0 * x2;
+    const c2: T = 1.0 / 2.0 * (x_1 + x1) - x0;
+    const c3: T = 1.0 / 6.0 * (x2 - x_1) + 1.0 / 2.0 * (x0 - x1);
+    return ((c3 * t + c2) * t + c1) * t + x0;
+}
+
+pub fn b_spline(comptime T: type, t: T, x_1: T, x0: T, x1: T, x2: T) T {
+    const ym1py1: T = x_1 + x1;
+    const c0: T = 1.0 / 6.0 * ym1py1 + 2.0 / 3.0 * x0;
+    const c1: T = 1.0 / 2.0 * (x1 - x_1);
+    const c2: T = 1.0 / 2.0 * ym1py1 - x0;
+    const c3: T = 1.0 / 2.0 * (x0 - x1) + 1.0 / 6.0 * (x2 - x_1);
+    return ((c3 * t + c2) * t + c1) * t + c0;
+}
